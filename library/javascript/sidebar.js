34733880d75e3cdf6206340defa995bf9274eaa7
Claudiu203 <mirel.claudiu2013@yahoo.ro>
2020-12-12 17:24:28 +0200

    5

--- library/javascript/sidebar.js
+++ library/javascript/sidebar.js
@@ -1,9 +1,15 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById("mySidenav").style.width = "66%";
+  }
+  function openNav2() {
+    document.getElementById("myNotification").style.width = "66%";
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+  }
+  function closeNav2() {
+    document.getElementById("myNotification").style.width = "0";
   }
