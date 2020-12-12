4296f6290ca38c38bf0494aca69e545889440089
David Pasarin <davidpasarinn@gmail.com>
2020-12-12 12:23:26 +0200

    Update index.php

--- library/initializare/index.php
+++ library/initializare/index.php
@@ -2,7 +2,7 @@
     $db = mysqli_connect('localhost', 'root', '');
     $query = "CREATE DATABASE `utilizatori-together`";
     mysqli_query($db, $query);
-    $query = "CREATE TABLE `utilizatori-together`.`utilizatori` ( `id` INT NULL DEFAULT NULL AUTO_INCREMENT , `username` TEXT NOT NULL , `email` TEXT NOT NULL , `parola` TEXT NOT NULL , `clasa` INT NOT NULL , `litera` CHAR(1) NOT NULL , `institutie` TEXT NOT NULL , `nastere` DATETIME NOT NULL , PRIMARY KEY (`id`), UNIQUE (`username`), UNIQUE (`email`)) ENGINE = InnoDB;";
+    $query = "CREATE TABLE `utilizatori-together`.`utilizatori` ( `id` INT NULL DEFAULT NULL AUTO_INCREMENT , `username` TEXT NOT NULL , `email` TEXT NOT NULL , `parola` TEXT NOT NULL , `clasa` INT NOT NULL , `litera` CHAR(1) NOT NULL , `institutie` TEXT NOT NULL , `nastere` DATE NOT NULL , PRIMARY KEY (`id`), UNIQUE (`username`), UNIQUE (`email`)) ENGINE = InnoDB;";
     mysqli_query($db, $query);
     header('Location: http://localhost');
 ?>
