f478a59b105b969870e3becd4742f7ca1183da39
David Pasarin <davidpasarinn@gmail.com>
2020-12-12 08:57:37 +0200

    basic
    basic

---
+++ library/funtions.php
@@ -0,0 +1,5 @@
+<?php
+
+$urlRoot = "localhost";
+
+?>
---
+++ library/post.php
@@ -0,0 +1,10 @@
+<div class="post-container">
+    <div class="account-info">
+        <div class="account-photo">
+            <img src="/library/imagini/profil_1.png" width="70" height="70" alt="">
+            <div class="account-post-info">
+                <span style="color: #00c98d">David Păsărin</span> a postat un nou anunț în <span style="color: #00c98d"><br> Informatică</span>.
+            </div>
+        </div>
+    </div>
+</div>
